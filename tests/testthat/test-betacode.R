9b9c42845de11267e99bfff067c2e66c97a3d0aa
Xavier Marquez <marquez.x@gmail.com>
2022-12-21 23:48:34 +1300

    New tests of strict argument

--- tests/testthat/test-betacode.R
+++ tests/testthat/test-betacode.R
@@ -1,10 +1,17 @@
 test_that("Incorrect betacode gives a warning", {
   expect_warning(betacode_to_unicode(paste("p*l*%a*t*o*n*", plato_republic$text[1])))
+  expect_warning(betacode_to_unicode("fh|\\s"))
+  expect_no_warning(betacode_to_unicode("fh|\\s", strict = FALSE))
 })
 
 test_that("Produces correct Unicode", {
   expect_equal(betacode_to_unicode("*sidw\\n e)pi\\ qala/tth| po/lis *)assuri/wn"),
                "Σιδὼν ἐπὶ θαλάττῃ πόλις Ἀσσυρίων")
+})
+
+test_that("Returns character vector of unicode vectors", {
+  expect_equal(length(betacode_to_unicode(plato_republic$text[1:10])),
+               10)
 })
 
 test_that("Returns character vector of unicode vectors", {
